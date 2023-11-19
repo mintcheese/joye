df9be141710f3b7ebd1e6546f2c9ccc6a1e34853
mintcheese <cindyzhouwy@gmail.com>
2023-11-19 19:53:09 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'bf8177d468cb4b2f914e350e34c6a332',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '刘祎祎个人网页',
+  domain: 'liuyiyi.xyz',
+  author: '刘祎祎',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '刘祎祎个人网页',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
